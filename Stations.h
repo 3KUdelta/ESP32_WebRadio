b00f7c3f0e7b7c099ac84d143a65123534d95a85
3KU_Delta <48127424+3KUdelta@users.noreply.github.com>
2022-07-01 23:28:32 +0200

    fixed a couple of stations

--- Stations.h
+++ Stations.h
@@ -1,38 +1,44 @@
-const int totalStations = 8;    // if you add/delete stations change this accordingly
+const int totalStations = 10;    // if you add/delete stations change this accordingly
+
+char *radioname[totalStations] = {  // Station names to be displayed on OLED (max 13 chars)
+  "Hit Radio FFH",
+  " FFH Lounge  ", 
+  " FFH Sommer  ",  
+  " Leider Geil ", 
+  "  Swiss Pop  ", 
+  " Deluxe Easy ",
+  "Absolut Chill",
+  " Energy Hits ",
+  "  Energy Me  ",
+  "NRJ on social"
+ };
 
 char *host[totalStations] = {
   "mp3.ffh.de", 
-  "iphone.live24.gr", 
   "mp3.ffh.de", 
   "mp3.ffh.de", 
   "mp3.ffh.de", 
-  "rautemusik-de-hz-fal-stream11.radiohost.de", 
   "stream.srg-ssr.ch", 
-  "deluxe.hoerradar.de"
+  "deluxe.hoerradar.de",
+  "ais-sa5.cdnstream1.com",
+  "energyhits.ice.infomaniak.ch",
+  "energymetime.ice.infomaniak.ch",
+  "energyspecial2.ice.infomaniak.ch"
 };
 
 char *path[totalStations] = { 
   "/radioffh/hqlivestream.mp3", 
-  "/radio998",
   "/ffhchannels/hqlounge.mp3", 
   "/ffhchannels/hqsummerfeeling.mp3", 
   "/ffhchannels/hqvoting.mp3", 
-  "/top40", 
   "/m/rsp/mp3_128", 
-  "/deluxe-easy-mp3-hq"
+  "/deluxe-easy-mp3-hq",
+  "/b05055_128mp3",
+  "/energyhits-high.mp3",
+  "/energymetime-high.mp3",
+  "/energyspecial2-high.mp3"
 };
 
 int port[totalStations] = {
-  80, 80, 80, 80, 80, 80, 80, 80
+  80, 80, 80, 80, 80, 80, 80, 80, 80, 80
 };
-
-char *radioname[totalStations] = {  // Station names to be displayed on OLED (max 13 chars)
-  "Hit Radio FFH",
-  "   Smooth    ",
-  " FFH Lounge  ", 
-  " FFH Sommer  ",  
-  " Leider Geil ", 
-  "   TOP 40    ", 
-  "  Swiss Pop  ", 
-  " Deluxe Easy "
- };
