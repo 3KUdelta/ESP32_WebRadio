022d6c62bc82c5d7dda82448f39d7177e16375bb
3KU_Delta <48127424+3KUdelta@users.noreply.github.com>
2022-07-04 20:24:02 +0200

    refactoring-adding ID3 metadata (song info)

--- ESP32_Web_Radio_AP.ino
+++ ESP32_Web_Radio_AP.ino
@@ -7,23 +7,24 @@
 //  - added rotary knob to change channels
 //  - added 128x32 oled display
 //  - stereo version
-//  - added treble and bass control
 //  - added WiFiManger to config WiFi credentials over an access point
 //  - added header file Stations.h for easier stations management
-//  Modified Juli 2022
+//  Modified in Juli 2022
 //  - Wifi Manager change
-//  - exchange dissapeared stations
+//  - corrected stations list
+//  - refactored code with ESP32_VS1053_Stream Library
+//  - adding Metadata such as song information
 
-#include <VS1053.h>           //https://github.com/baldram/ESP_VS1053_Library
-#include <ESP32Encoder.h>     //https://github.com/madhephaestus/ESP32Encoder
+#include <VS1053.h>               //https://github.com/baldram/ESP_VS1053_Library
+#include <ESP32_VS1053_Stream.h>  //https://github.com/CelliesProjects/ESP32_VS1053_Stream
+#include <ESP32Encoder.h>         //https://github.com/madhephaestus/ESP32Encoder
 #include <HTTPClient.h>
 #include <EEPROM.h>
-#include <U8g2lib.h>          //https://github.com/olikraus/u8g2
+#include <U8g2lib.h>              //https://github.com/olikraus/u8g2
 #include <Wire.h>
 #include <esp_wifi.h>
-#include <HTTPClient.h>
-#include <WiFiManager.h>      //https://github.com/tzapu/WiFiManager
-#include "Stations.h"         // Radio station settings
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+#include "Stations.h"             // Radio station settings
 
 #define VS1053_CS    32
 #define VS1053_DCS   33
@@ -32,13 +33,7 @@
 #define VOLUME       95         // volume level 0-100
 #define EEPROM_SIZE  64
 
-//   Bass/Treble: void setTone(uint8_t *rtone);
-//
-//   toneha       = <0..15>        // Setting treble gain (0 off, 1.5dB steps)
-//   tonehf       = <0..15>        // Setting treble frequency lower limit x 1000 Hz
-//   tonela       = <0..15>        // Setting bass gain (0 = off, 1dB steps)
-//   tonelf       = <0..15>        // Setting bass frequency lower limit x 10 Hz
-uint8_t rtone[4]  = {1, 20, 10, 2}; // initialize bass & treble
+ESP32_VS1053_Stream stream;
 
 //***** RotaryEncoder
 const int rotaryDT  = 16;
@@ -56,21 +51,16 @@ long previousMillis = 0;
 
 int radioStation = 0;
 int previousRadioStation = -1;
-
-WiFiClient client;
-
-uint8_t mp3buff[64];   // vs1053 likes 32 bytes at a time
-VS1053 player(VS1053_CS, VS1053_DCS, VS1053_DREQ);
-
-//void(* resetFunc) (void) = 0;       // declare reset function @ address 0
+String songinfo;
+int shift = 128;
+int textwidth;
+unsigned int currentState = 0;  // variable for statemachine in loop
 
 void setup () {
 
   Serial.begin(115200); while (!Serial); delay(200);
   Serial.println("Starting up Web Radio");
   delay(500);
-  Serial.println("initializing oled screen");
-  delay(1000);
 
   SPI.begin();
 
@@ -79,7 +69,7 @@ void setup () {
   u8g2.setFont(u8g2_font_t0_11_t_all);    // https://github.com/olikraus/u8g2/wiki/fntlistall for fonts
   u8g2.drawStr(2, 22, "Starting engine");
   u8g2.sendBuffer();
-  delay(500);
+  delay(2000);                            // give enough time to powerup peripherals
 
   Serial.println("Connecting to WiFi");
   u8g2.clearBuffer();
@@ -107,10 +97,7 @@ void setup () {
   u8g2.clearBuffer();
   u8g2.drawStr(2, 22, "Decoder setup");
   u8g2.sendBuffer();
-  player.begin();
-  player.switchToMp3Mode(); // make sure it is not in MIDI mode
-  player.setVolume(VOLUME);
-  player.setTone(rtone);
+  stream.startDecoder(VS1053_CS, VS1053_DCS, VS1053_DREQ);
 
   Serial.println("reading last station from eeprom");
   radioStation = readStationFromEEPROM();
@@ -119,10 +106,11 @@ void setup () {
   encoder.setCount(radioStation);  // setting rotary encoder to station number
 
   Serial.println("setup done");
+  u8g2.setFont(u8g2_font_profont17_mr);
 }
 
 void loop() {
-
+  stream.loop();
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis > interval)
   {
@@ -139,8 +127,9 @@ void loop() {
       encoder.setCount(radioStation);
     }
 
-    if (radioStation != previousRadioStation)
+    if (radioStation != previousRadioStation)   // we are changing the station
     {
+      currentState = 0;
       station_connect(radioStation);
       Serial.print("Number of station: ");
       Serial.println(radioStation);
@@ -159,34 +148,53 @@ void loop() {
       }
     }
     previousMillis = currentMillis;
-    u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_profont17_mr);
-    u8g2.setCursor(7, 22);
-    u8g2.print (String(radioname[radioStation]));
-    u8g2.drawLine(0, 0, 127, 0);
-    u8g2.drawLine(0, 31, 127, 31);
-    u8g2.sendBuffer();
 
-    if ((!digitalRead(rotarySW))) {
-      ESP.restart();   // button pressed, doing a restart
-    }
+  }
+  if ((!digitalRead(rotarySW))) {
+    ESP.restart();   // button pressed, doing a restart
   }
 
-  if (client.available() > 0)
-  {
-    uint8_t bytesread = client.read(mp3buff, 64);
-    player.playChunk(mp3buff, bytesread);
+  switch (currentState) {                          // non-blocking statemachine to allow button interrupt
+    case 0:
+      u8g2.clearBuffer();
+      u8g2.setCursor(7, 22);
+      u8g2.print(String(radioname[radioStation]));
+      u8g2.drawLine(0, 0, 127, 0);
+      u8g2.drawLine(0, 31, 127, 31);
+      u8g2.sendBuffer();
+      currentState++;
+      break;
+    case 1:
+      if (wait(3000) == true)
+      {
+        currentState++;        // if lapsed, go to next step (state)
+        shift = 128;           // start at the right end of the screen (for case 2)
+      }
+      break;
+    case 2:
+      u8g2.firstPage();
+      do {
+        u8g2.setCursor(shift--, 22);
+        u8g2.print(songinfo);
+        u8g2.drawLine(0, 0, 127, 0);
+        u8g2.drawLine(0, 31, 127, 31);
+      } while ( u8g2.nextPage() );
+      if (shift == textwidth * -1) currentState = 0;
+      break;
   }
 }
 
 void station_connect (int station_no ) {
-  if (client.connect(host[station_no], port[station_no])) {
-    Serial.print("Station connected : ");
-    Serial.println(String(radioname[radioStation]));
-  }
-  client.print(String("GET ") + path[station_no] + " HTTP/1.1\r\n" +
-               "Host: " + host[station_no] + "\r\n" +
-               "Connection: close\r\n\r\n");
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_profont17_mr);
+  u8g2.setCursor(7, 22);
+  u8g2.print ("buffering ...");
+  u8g2.drawLine(0, 0, 127, 0);
+  u8g2.drawLine(0, 31, 127, 31);
+  u8g2.sendBuffer();
+  stream.stopSong();
+  Serial.println(String(radioname[radioStation]));
+  stream.connecttohost(host[radioStation]);
 }
 
 void configModeCallback (WiFiManager * myWiFiManager) {
@@ -230,4 +238,35 @@ void writeStationToEEPROM()
   Serial.println("Saving radio station to EEPROM: " + String(radioStation));
   EEPROM.write(0, radioStation);
   EEPROM.commit();
+}
+void audio_showstation(const char* info) {
+  Serial.printf("showstation: %s\n", info);
+}
+
+void audio_showstreamtitle(const char* info) {
+  Serial.printf("streamtitle: %s\n", info);
+  songinfo = info;
+  textwidth = u8g2.getUTF8Width(info);    // calculate the pixel width of the text
+}
+
+void audio_eof_stream(const char* info) {
+  Serial.printf("eof: %s\n", info);
+}
+bool wait(unsigned long duration)
+{
+  static unsigned long startTime;
+  static bool isStarted = false;
+
+  if (isStarted == false)                  // if wait period not started yet
+  {
+    startTime = millis();                  // set the start time of the wait period
+    isStarted = true;                      // indicate that it's started
+    return false;                          // indicate to caller that wait period is in progress
+  }
+  if (millis() - startTime >= duration)    // check if wait period has lapsed
+  {
+    isStarted = false;    // lapsed, indicate no longer started so next time we call the function it will initialise the start time again
+    return true;          // indicate to caller that wait period has lapsed
+  }
+  return false;           // indicate to caller that wait period is in progress
 }
